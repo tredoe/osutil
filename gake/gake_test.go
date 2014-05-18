7693d90c951c1b580056370b80a392bca4afc7d2
Jonas mg <jonas@matx.cc>
2014-05-18 20:14:55 +0100

    gake: use goutil.TestingCmd for testing commands

--- gake/gake_test.go
+++ gake/gake_test.go
@@ -7,33 +7,17 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/exec"
 	"testing"
 
 	"github.com/kless/goutil"
 )
 
-const CMD_MAIN = "test-gake"
-
-// Build the command
-func init() {
-	err := exec.Command("go", "build", "-o", CMD_MAIN).Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func TestCommand(t *testing.T) {
-	err := exec.Command("./"+CMD_MAIN, "testdata").Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestMultiPackages(t *testing.T) {
 	commandTests := []goutil.CommandTest{
+		{
+			Args: "./testdata/",
+			Out:  "",
+		},
 		{
 			Args:   "./testdata/multi_pkg/",
 			Stderr: `can't load package: found packages "main2" ('testdata/multi_pkg/test3_make.go', 'testdata/multi_pkg/test2_make.go'), "main" ('testdata/multi_pkg/test1_make.go') in './testdata/multi_pkg/'` + "\n",
@@ -44,12 +28,5 @@ func TestMultiPackages(t *testing.T) {
 	err := goutil.TestingCmd(".", commandTests)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	// Remove the commands
-	for _, v := range []string{CMD_MAIN} {
-		if err = os.Remove(v); err != nil {
-			t.Log(err)
-		}
 	}
 }
